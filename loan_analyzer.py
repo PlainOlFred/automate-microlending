5763a64d70db9841f282203ab0ebfb427610fa0a
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2023-05-15 21:24:29 -0500

    write inexpensive loans from list of loans

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -13,6 +13,9 @@ number_of_loans = len(loan_costs)
 loan_total = sum(loan_costs)
 average_loan = sum(loan_costs) / len(loan_costs)
 
+#To hold that cost $500 or less
+inexpensive_loans = []
+
 if(number_of_loans > 1):
   print(f"Analyzing {number_of_loans} loans for a total value of ${loan_total}. Average loan amount ${average_loan}")
 elif (number_of_loans == 1): 
@@ -20,25 +23,71 @@ elif (number_of_loans == 1):
 else: 
   print(f"No loans to be Analyzed")
 
-
-# Test loan data for calculating the present value for the loan
-loan = {
-    "loan_price": 500,
-    "remaining_months": 9,
-    "repayment_interval": "bullet",
-    "future_value": 1000,
-}
-
 # Discount Rate
 annual_discount_rate = 0.20
 
-future_value = loan.get("future_value")
-remaining_months = loan.get("remaining_months")
-cost = loan.get("loan_price")
+# Test loans
+loans = [
+    {
+        "loan_price": 700,
+        "remaining_months": 9,
+        "repayment_interval": "monthly",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 500,
+        "remaining_months": 13,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 200,
+        "remaining_months": 16,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 900,
+        "remaining_months": 16,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+    {
+      "loan_price": 500,
+      "remaining_months": 9,
+      "repayment_interval": "bullet",
+      "future_value": 1000,
+  }
+]
 
-present_value = calculate_present_value(future_value, remaining_months, annual_discount_rate) 
+# gather loans less than 500 in inexpensive_loans
+for loan in loans:
+  if loan["loan_price"] <= 500:
+    inexpensive_loans.append(loan)
 
-if present_value >= cost:
-  print("Yes, the loan is worth at least the cost to buy it.")
-else:
-    print("No, the loan is too expensive and not worth the price.")
+  # calculate present value
+  future_value = loan.get("future_value")
+  remaining_months = loan.get("remaining_months")
+  cost = loan.get("loan_price")
+
+  present_value = calculate_present_value(future_value, remaining_months, annual_discount_rate) 
+
+  if present_value >= cost:
+    print("Yes, the loan is worth at least the cost to buy it.")
+  else:
+      print("No, the loan is too expensive and not worth the price.")
+
+print(f"The inexpensive loans are: {inexpensive_loans}")
+
+# Write inexpensive loans report 
+# Report headers
+header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
+
+# Report Path
+output_path = Path("inexpensive_loans.csv")
+
+with open(output_path, 'w') as csvfile:
+  csvwriter = csv.writer(csvfile, delimiter=',')
+  csvwriter.writerow(header)
+  for loan in inexpensive_loans:
+    csvwriter.writerow(loan.values())
