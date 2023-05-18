377182d83ef1c8958c5bc121e8704c9dab1f828b
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2023-05-17 19:16:14 -0500

    move loan summary logic into loans analysis

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -6,26 +6,6 @@ def calculate_present_value(future_value, remaining_months, annual_discount_rate
   present_value = future_value / (1 + (annual_discount_rate / 12)) ** remaining_months
   return present_value
 
-# Loan prices to be analyze
-loan_costs = [500, 600, 200, 1000, 450]
-
-number_of_loans = len(loan_costs)
-loan_total = sum(loan_costs)
-average_loan = sum(loan_costs) / len(loan_costs)
-
-#To hold that cost $500 or less
-inexpensive_loans = []
-
-if(number_of_loans > 1):
-  print(f"Analyzing {number_of_loans} loans for a total value of ${loan_total}. Average loan amount ${average_loan}")
-elif (number_of_loans == 1): 
-  print(f"Analyzing loan for a total value of ${loan_total}. Average loan amount ${average_loan}")
-else: 
-  print(f"No loans to be Analyzed")
-
-# Discount Rate
-annual_discount_rate = 0.20
-
 # Test loans
 loans = [
     {
@@ -59,9 +39,36 @@ loans = [
       "future_value": 1000,
   }
 ]
+current_loan = 0
+
+
+# Loan prices to be analyze
+loan_costs = [loan.get('loan_price') for loan in loans ]
+
+number_of_loans = len(loan_costs)
+loan_total = sum(loan_costs)
+average_loan = sum(loan_costs) / len(loan_costs)
+
+#To hold that cost $500 or less
+inexpensive_loans = []
+
+if(number_of_loans > 1):
+  print(f"Analyzing {number_of_loans} loans for a total value of ${loan_total}. Average loan amount ${average_loan}")
+elif (number_of_loans == 1): 
+  print(f"Analyzing loan for a total value of ${loan_total}. Average loan amount ${average_loan}")
+else: 
+  print(f"No loans to be Analyzed")
+
+# Discount Rate
+annual_discount_rate = 0.20
+
 
 # gather loans less than 500 in inexpensive_loans
 for loan in loans:
+  # increase current loan count
+  current_loan = current_loan + 1
+
+  # check for inexpensive loan
   if loan["loan_price"] <= 500:
     inexpensive_loans.append(loan)
 
@@ -73,7 +80,7 @@ for loan in loans:
   present_value = calculate_present_value(future_value, remaining_months, annual_discount_rate) 
 
   if present_value >= cost:
-    print("Yes, the loan is worth at least the cost to buy it.")
+    print(f"Yes, loan number {} is worth at least the cost to buy it.")
   else:
       print("No, the loan is too expensive and not worth the price.")
 
