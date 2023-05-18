302269ed583121d190819750b5921b7edf8f9ee6
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2023-05-17 20:06:12 -0500

    seperate loan analyze functionality into separate functions \

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -6,6 +6,67 @@ def calculate_present_value(future_value, remaining_months, annual_discount_rate
   present_value = future_value / (1 + (annual_discount_rate / 12)) ** remaining_months
   return present_value
 
+def write_loans_to_report(file, loans_to_write):
+  '''Wrie loan report to file'''
+  # Report Path
+  output_path = Path(file)
+  headers = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
+
+  with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(headers)
+    for loan in loans_to_write:
+      csvwriter.writerow(loan.values())
+  return 0
+
+def analyze_loans(loans, annual_discount_rate):
+  '''Analyze loan - print cost summary; compare present value; write to inexpensive loan report'''
+  loan_costs = [loan.get('loan_price') for loan in loans ]
+
+  number_of_loans = len(loan_costs)
+  loan_total = sum(loan_costs)
+  average_loan = sum(loan_costs) / len(loan_costs)
+
+  # Print Loan Summary
+  if(number_of_loans > 1):
+    print(f"Analyzing {number_of_loans} loans for a total value of ${loan_total}. Average loan amount ${average_loan}")
+  elif (number_of_loans == 1): 
+    print(f"Analyzing loan for a total value of ${loan_total}. Average loan amount ${average_loan}")
+  else: 
+    print(f"No loans to be Analyzed")
+
+  #To hold that cost $500 or less
+  inexpensive_loans = []
+  
+  loan_count = 0
+
+  # gather loans less than 500 in inexpensive_loans
+  for loan in loans:
+    # increase current loan count
+    loan_count = loan_count + 1
+  
+    # check for inexpensive loan
+    if loan["loan_price"] <= 500:
+      inexpensive_loans.append(loan)
+
+    # calculate present value
+    future_value = loan.get("future_value")
+    remaining_months = loan.get("remaining_months")
+    cost = loan.get("loan_price")
+
+    present_value = calculate_present_value(future_value, remaining_months, annual_discount_rate) 
+
+    if present_value >= cost:
+      print(f"Yes, loan number {loan_count} is worth at least the cost to buy it.")
+    else:
+        print(f"No, loan number {loan_count} is too expensive and not worth the price.")
+
+  print(f"The inexpensive loans are: {inexpensive_loans}")
+  # write inexpensive loan report
+  write_loans_to_report("inexpensive_loans.csv", inexpensive_loans)
+
+  return 0
+
 # Test loans
 loans = [
     {
@@ -39,62 +100,6 @@ loans = [
       "future_value": 1000,
   }
 ]
-current_loan = 0
 
 
-# Loan prices to be analyze
-loan_costs = [loan.get('loan_price') for loan in loans ]
-
-number_of_loans = len(loan_costs)
-loan_total = sum(loan_costs)
-average_loan = sum(loan_costs) / len(loan_costs)
-
-#To hold that cost $500 or less
-inexpensive_loans = []
-
-if(number_of_loans > 1):
-  print(f"Analyzing {number_of_loans} loans for a total value of ${loan_total}. Average loan amount ${average_loan}")
-elif (number_of_loans == 1): 
-  print(f"Analyzing loan for a total value of ${loan_total}. Average loan amount ${average_loan}")
-else: 
-  print(f"No loans to be Analyzed")
-
-# Discount Rate
-annual_discount_rate = 0.20
-
-
-# gather loans less than 500 in inexpensive_loans
-for loan in loans:
-  # increase current loan count
-  current_loan = current_loan + 1
-
-  # check for inexpensive loan
-  if loan["loan_price"] <= 500:
-    inexpensive_loans.append(loan)
-
-  # calculate present value
-  future_value = loan.get("future_value")
-  remaining_months = loan.get("remaining_months")
-  cost = loan.get("loan_price")
-
-  present_value = calculate_present_value(future_value, remaining_months, annual_discount_rate) 
-
-  if present_value >= cost:
-    print(f"Yes, loan number {} is worth at least the cost to buy it.")
-  else:
-      print("No, the loan is too expensive and not worth the price.")
-
-print(f"The inexpensive loans are: {inexpensive_loans}")
-
-# Write inexpensive loans report 
-# Report headers
-header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
-
-# Report Path
-output_path = Path("inexpensive_loans.csv")
-
-with open(output_path, 'w') as csvfile:
-  csvwriter = csv.writer(csvfile, delimiter=',')
-  csvwriter.writerow(header)
-  for loan in inexpensive_loans:
-    csvwriter.writerow(loan.values())
+analyze_loans(loans, annual_discount_rate=0.20)
